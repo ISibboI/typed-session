72de706be8b303557ce497d1b98acb6969e6a368
Sebastian Schmidt <isibboi@gmail.com>
2023-09-24 10:53:14 +0300

    Use `thiserror` to implement errors.

--- src/error.rs
+++ src/error.rs
@@ -1,19 +1,25 @@
-use std::fmt::{Debug, Display, Formatter};
+use std::fmt::Debug;
 
 /// All errors that can occur in this crate.
-#[derive(Debug)]
+#[derive(Debug, thiserror::Error)]
 #[allow(missing_copy_implementations)]
 pub enum Error<SessionStoreConnectorError> {
     /// A session was attempted to be updated, but the session does not exist.
     /// This may happen due to concurrent modification, and is forbidden to prevent data inconsistencies.
     /// If you receive this error, revert everything that you did while handling the request that
     /// used this session.
+    #[error("the session that was attempted to be updated does not exist, which indicates that it was concurrently modified or deleted")]
     UpdatedSessionDoesNotExist,
 
     /// Tried as often as desired to generate a session id, but all generated ids already exist.
-    MaximumSessionIdGenerationTriesReached,
+    #[error("the maximum number of retries to generate a session id was reached")]
+    MaximumSessionIdGenerationTriesReached {
+        /// The maximum number of retries that was reached.
+        maximum: u32,
+    },
 
     /// The given cookie has a wrong length.
+    #[error("the given cookie has length {actual}, but is expected to have length {expected}")]
     WrongCookieLength {
         /// The expected cookie length.
         expected: usize,
@@ -22,6 +28,7 @@ pub enum Error<SessionStoreConnectorError> {
     },
 
     /// An error occurred in the session store connector.
+    #[error("{0}")]
     SessionStoreConnector(SessionStoreConnectorError),
 }
 
@@ -33,7 +40,7 @@ impl<SessionStoreConnectorError> From<SessionStoreConnectorError>
     }
 }
 
-impl<SessionStoreConnectorError: Display> Display for Error<SessionStoreConnectorError> {
+/*impl<SessionStoreConnectorError: Display> Display for Error<SessionStoreConnectorError> {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
             Error::UpdatedSessionDoesNotExist => write!(f, "the updated session does not exist, which indicates that it was concurrently modified or deleted."),
@@ -42,9 +49,13 @@ impl<SessionStoreConnectorError: Display> Display for Error<SessionStoreConnecto
             Error::SessionStoreConnector(error) => write!(f, "{error}"),
         }
     }
-}
+}*/
 
-impl<SessionStoreConnectorError: Debug + Display> std::error::Error
-    for Error<SessionStoreConnectorError>
-{
+mod expect_impl_error {
+    trait ExpectImplError: std::error::Error {}
+
+    impl<SessionStoreConnectorError: std::error::Error> ExpectImplError
+        for super::Error<SessionStoreConnectorError>
+    {
+    }
 }
